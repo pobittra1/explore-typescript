8974de1f1be4413d0b6d2ab4b0edfaac580a9c0f
pobi <pobittrachandraroy2001@gmail.com>
2024-05-01 18:18:02 +0600

    explore about never, unknown and nullable types

---
+++ module1/src/1.12.ts
@@ -0,0 +1,37 @@
+{
+  //never, unknown and nullable type
+
+  //nullable type
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("searching");
+    } else {
+      console.log("there is nothing to search");
+    }
+  };
+  searchName(null);
+
+  //unknown type
+  const getSpeedInMeterPerSecond = (val: unknown) => {
+    // there is unknown type
+    if (typeof val === "number") {
+      const convertedSpeed = (val * 1000) / 3600;
+      console.log(`the speed is ${convertedSpeed} ms^-1`);
+    } else if (typeof val === "string") {
+      const [value, unit] = val.split(" ");
+      const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+      console.log(`the speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("wrong input");
+    }
+  };
+
+  getSpeedInMeterPerSecond(`1000 ms^-1`);
+
+  //never type
+
+  const throwError = (msg: string) => {
+    throw new Error(msg);
+  };
+  throwError("your code is not working");
+}
