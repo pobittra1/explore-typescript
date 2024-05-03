083e60ada925def5e48f78db247b0401a7203be1
pobi <pobittrachandraroy2001@gmail.com>
2024-05-03 10:47:24 +0600

    explore about constraints in ts

---
+++ ts-advance/constraints-in-ts.ts
@@ -0,0 +1,40 @@
+{
+  //contraints in ts (thats mean it's fixed value when i pass dynamic data)
+  //create dynamic type and return two value into object with spread operator
+
+//   //as my opinion
+//   interface Student {
+//     id: number;
+//     name: string;
+//     email: string;
+//   }
+//   const addCourseToStudent = <T extends Student>(studentData: T) => {
+//     const course = "course of data structure and algorithm";
+//     return {
+//       ...studentData,
+//       course,
+//     };
+//   };
+    const addCourseToStudent = <
+      T extends {
+        //constraints part
+        id: number;
+        name: string;
+        email: string;
+      }
+    >(
+      studentData: T
+    ) => {
+      const course = "course of data structure and algorithm";
+      return {
+        ...studentData,
+        course,
+      };
+    };
+
+  const student1 = addCourseToStudent<Student>({
+    id: 1,
+    name: "something",
+    email: "example@email.com",
+  });
+}
