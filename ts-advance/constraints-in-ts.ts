63d7e20b92fe89e88359dd13d5b755dc31d6d7ab
pobi <pobittrachandraroy2001@gmail.com>
2024-05-03 11:00:19 +0600

    solve error

--- ts-advance/constraints-in-ts.ts
+++ ts-advance/constraints-in-ts.ts
@@ -2,37 +2,41 @@
   //contraints in ts (thats mean it's fixed value when i pass dynamic data)
   //create dynamic type and return two value into object with spread operator
 
-//   //as my opinion
-//   interface Student {
-//     id: number;
-//     name: string;
-//     email: string;
-//   }
-//   const addCourseToStudent = <T extends Student>(studentData: T) => {
-//     const course = "course of data structure and algorithm";
-//     return {
-//       ...studentData,
-//       course,
-//     };
-//   };
-    const addCourseToStudent = <
-      T extends {
-        //constraints part
-        id: number;
-        name: string;
-        email: string;
-      }
-    >(
-      studentData: T
-    ) => {
-      const course = "course of data structure and algorithm";
-      return {
-        ...studentData,
-        course,
-      };
+  //   //as my opinion
+  //   interface Student {
+  //   id: number;
+  //   name: string;
+  //   email: string;
+  //   }
+  //   const addCourseToStudent = <T extends Student>(studentData: T) => {
+  //     const course = "course of data structure and algorithm";
+  //     return {
+  //       ...studentData,
+  //       course,
+  //     };
+  //   };
+  const addCourseToStudent = <
+    T extends {
+      //constraints part
+      id: number;
+      name: string;
+      email: string;
+    }
+  >(
+    studentData: T
+  ) => {
+    const course = "course of data structure and algorithm";
+    return {
+      ...studentData,
+      course,
     };
+  };
 
-  const student1 = addCourseToStudent<Student>({
+  const student1 = addCourseToStudent<{
+    id: number;
+    name: string;
+    email: string;
+  }>({
     id: 1,
     name: "something",
     email: "example@email.com",
